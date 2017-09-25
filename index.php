507021d836d40caf0209f4b63abfa998e72b6288
tockorea <lim.chae.eun@gmail.com>
2017-09-26 00:24:13 +0900

    bootstrap 적용

--- index.php
+++ index.php
@@ -14,60 +14,69 @@
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
   </head>
   <body id="target">
-    <header>
-      <h1><a href="index.php">Web Application</a></h1>
-    </header>
-    <nav>
-      <ol>
-        <?php
-          // // list.txt 파일을 읽어서 출력한다.
-          // echo file_get_contents('list.txt');
-          // 텍스트파일 → MySQL로 변경
-          while ($row = mysqli_fetch_assoc($result)) {
-            echo '<li><a href="index.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
-          }
-         ?>
-      </ol>
-    </nav>
-    <div id="control">
-      <input type="button" value="black" onclick="
-        $('#target').attr('class', 'black');
-      ">
-      <!-- javascript를 jquery로 변경 -->
-      <!-- document.getElementById('target').className = 'black'; -->
-      <!-- $('#target').attr('class', 'black'); -->
-      <input type="button" value="white" onclick="
-        $('#target').attr('class', 'white');
-      ">
-      <!-- javascript를 jquery로 변경 -->
-      <!-- document.getElementById('target').className = 'white'; -->
-      <!-- $('#target').attr('class', 'white'); -->
-      <a href="write.php">쓰기</a>
+    <div class="container-fluid">
+      <header class="hero-unit">
+        <h1><a href="index.php">Web Application</a></h1>
+      </header>
+      <div class="row-fluid">
+        <nav class="span3">
+          <ol class="nav nav-pills nav-stacked">
+            <?php
+              // // list.txt 파일을 읽어서 출력한다.
+              // echo file_get_contents('list.txt');
+              // 텍스트파일 → MySQL로 변경
+              while ($row = mysqli_fetch_assoc($result)) {
+                echo '<li><a href="index.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
+              }
+             ?>
+          </ol>
+        </nav>
+        <div class="span9">
+          <article>
+            <?php
+              // // 만약 id 파라미터의 값이 있다면
+              // if (isset($_GET['id'])) {
+              //   // id 파라미터를 이용해서 파일명을 만든다.
+              //   $filename = $_GET['id'].'.txt';
+              //   // 파일명의 파일을 읽어서 출력한다.
+              //   echo file_get_contents($filename);
+              // } else {
+              // 텍스트파일 → MySQL로 변경
+              if (empty($_GET['id']) === false) {
+                $sql = 'SELECT * FROM topic WHERE id = '.$_GET['id'];
+                $result = mysqli_query($conn, $sql);
+                $row = mysqli_fetch_assoc($result);
+                echo '<h2>'.$row['title'].'</h2>';
+                echo $row['description'];
+              } else {
+             ?>
+                <h2>Welcom</h2>
+                Happy coding!!
+            <?php
+              }
+             ?>
+          </article>
+          <hr>
+          <div id="control">
+            <div class="btn-group">
+              <input type="button" value="black" onclick="
+                $('#target').attr('class', 'black');
+              " class="btn btn-default btn-large">
+              <!-- javascript를 jquery로 변경 -->
+              <!-- document.getElementById('target').className = 'black'; -->
+              <!-- $('#target').attr('class', 'black'); -->
+              <input type="button" value="white" onclick="
+                $('#target').attr('class', 'white');
+              " class="btn btn-default btn-large">
+              <!-- javascript를 jquery로 변경 -->
+              <!-- document.getElementById('target').className = 'white'; -->
+              <!-- $('#target').attr('class', 'white'); -->
+            </div>
+            <a href="write.php" class="btn btn-success btn-large">쓰기</a>
+          </div>
+        </div>
+      </div>
     </div>
-    <article>
-      <?php
-        // // 만약 id 파라미터의 값이 있다면
-        // if (isset($_GET['id'])) {
-        //   // id 파라미터를 이용해서 파일명을 만든다.
-        //   $filename = $_GET['id'].'.txt';
-        //   // 파일명의 파일을 읽어서 출력한다.
-        //   echo file_get_contents($filename);
-        // } else {
-        // 텍스트파일 → MySQL로 변경
-        if (empty($_GET['id']) === false) {
-          $sql = 'SELECT * FROM topic WHERE id = '.$_GET['id'];
-          $result = mysqli_query($conn, $sql);
-          $row = mysqli_fetch_assoc($result);
-          echo '<h2>'.$row['title'].'</h2>';
-          echo $row['description'];
-        } else {
-       ?>
-          <h2>Welcom</h2>
-          Happy coding!!
-      <?php
-        }
-       ?>
-    </article>
     <!-- Bootstrap -->
     <script src="jquery-3.2.1.js"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
--- write.php
+++ write.php
@@ -14,44 +14,63 @@
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
   </head>
   <body id="target">
-    <header>
-      <h1><a href="index.php">Web Application</a></h1>
-    </header>
-    <nav>
-      <ol>
-        <?php
-          // // list.txt 파일을 읽어서 출력한다.
-          // echo file_get_contents('list.txt');
-          // 텍스트파일 → MySQL로 변경
-          while ($row = mysqli_fetch_assoc($result)) {
-            echo '<li><a href="index.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
-          }
-         ?>
-      </ol>
-    </nav>
-    <div id="control">
-      <input type="button" value="black" onclick="
-        $('#target').attr('class', 'black');
-      ">
-      <!-- javascript를 jquery로 변경 -->
-      <!-- document.getElementById('target').className = 'black'; -->
-      <!-- $('#target').attr('class', 'black'); -->
-      <input type="button" value="white" onclick="
-        $('#target').attr('class', 'white');
-      ">
-      <!-- javascript를 jquery로 변경 -->
-      <!-- document.getElementById('target').className = 'white'; -->
-      <!-- $('#target').attr('class', 'white'); -->
-      <a href="write.php">쓰기</a>
+    <div class="container-fluid">
+      <header class="hero-unit">
+        <h1><a href="index.php">Web Application</a></h1>
+      </header>
+      <div class="row-fluid">
+        <nav class="span3">
+          <ol class="nav nav-pills nav-stacked">
+            <?php
+              // // list.txt 파일을 읽어서 출력한다.
+              // echo file_get_contents('list.txt');
+              // 텍스트파일 → MySQL로 변경
+              while ($row = mysqli_fetch_assoc($result)) {
+                echo '<li><a href="index.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
+              }
+             ?>
+          </ol>
+        </nav>
+        <div class="span9">
+          <article>
+            <form action="process.php" method="post">
+              <div class="form-group">
+                <label for="form-title">제목</label>
+                <input type="text" class="form-control span9" name="title" id="form-title" placeholder="제목을 적어주세요.">
+              </div>
+              <div class="form-group">
+                <label for="form-author">작성자</label>
+                <input type="text" class="form-control span9" name="author" id="form-author" placeholder="작성자를 적어주세요.">
+              </div>
+              <div class="form-group">
+                <label for="form-description">본문</label>
+                <textarea type="text" rows="10" class="form-control span9" name="description" id="form-description" placeholder="본문을 적어주세요.">
+                </textarea>
+              </div>
+              <input type="submit" name="제출" class="btn btn-default btn-large">
+            </form>
+          </article>
+          <hr>
+          <div id="control">
+            <div class="btn-group">
+              <input type="button" value="black" onclick="
+                $('#target').attr('class', 'black');
+              " class="btn btn-default btn-large">
+              <!-- javascript를 jquery로 변경 -->
+              <!-- document.getElementById('target').className = 'black'; -->
+              <!-- $('#target').attr('class', 'black'); -->
+              <input type="button" value="white" onclick="
+                $('#target').attr('class', 'white');
+              " class="btn btn-default btn-large">
+              <!-- javascript를 jquery로 변경 -->
+              <!-- document.getElementById('target').className = 'white'; -->
+              <!-- $('#target').attr('class', 'white'); -->
+            </div>
+            <a href="write.php" class="btn btn-success btn-large">쓰기</a>
+          </div>
+        </div>
+      </div>
     </div>
-    <article>
-      <form action="process.php" method="post">
-        <p>제목 : <input type="text" name="title"></p>
-        <p>작성자 : <input type="text" name="author"></p>
-        <p>본문 : <textarea name="description"></textarea></p>
-        <input type="submit" name="제출">
-      </form>
-    </article>
     <!-- Bootstrap -->
     <script src="jquery-3.2.1.js"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
